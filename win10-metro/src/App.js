e78b973091c14b49e8a7f7806eb6388423630bb7
Mxtzz <mazw2016@hotmail.com>
2018-10-14 08:00:44 +0800

    Start a Windows10

---
+++ win10-metro/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Desktop from './pages/Desktop.js';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Desktop />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ win10-metro/src/pages/Desktop.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../css/desktop.css';
+
+class Desktop extends Component {
+    render() {
+        return (
+            <div className="desktop">
+                
+                <div className="stateBar">
+                    <div className="startBtn"></div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Desktop;
