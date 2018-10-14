a0efdcd305427873d44fc522ff219a796159354f
Mxtzz <mazw2016@hotmail.com>
2018-10-14 10:09:26 +0800

    add favicon

--- win10-metro/src/App.js
+++ win10-metro/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Desktop from './pages/Desktop.js';
+// import Desktop from './pages/Desktop.js';
 import Update from './pages/Update.js';
 
 class App extends Component {
